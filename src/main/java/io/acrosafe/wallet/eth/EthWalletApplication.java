034f17ec30b373881f810a5ff9088a8dde7ec9ef
ming.zhao <ming@acrosafe.io>
2020-03-01 20:24:13 -0500

    acro-102: initialized the project

---
+++ src/main/java/io/acrosafe/wallet/eth/EthWalletApplication.java
@@ -0,0 +1,15 @@
+package io.acrosafe.wallet.eth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthWalletApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EthWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/acrosafe/wallet/eth/EthWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package io.acrosafe.wallet.eth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
